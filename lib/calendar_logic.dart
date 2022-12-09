9e52e8881f978f0c2be1a8621b46606938981e91
natsuto-kun <natsuto391@gmail.com>
2022-12-09 11:44:06 +0900

    first commit

---
+++ lib/calendar_logic.dart
@@ -0,0 +1,10 @@
+library calendar_logic;
+
+/// A Calculator.
+class Calculator {
+  /// [date]が表す日が所属する月のカレンダーを生成
+  List<List<int?>> build(DateTime date) {
+    final calendar = <List<int?>>[];
+    return calendar;
+  }
+}
